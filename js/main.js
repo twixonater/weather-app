b53bd0765f51724287be2da53f04faf4e369f954
samotwix <36455283+twixonater@users.noreply.github.com>
2018-03-13 18:36:50 -0400

    updated based on comments and work with Brian.
    -Added .value information
    -Updated ./ instead of ../
    -updated the position of the variable ctemp

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 // Temperature F is main item we are looking for paired with background.
 var temperature;
-var ctemp = parseInt(document.getElementById("celsiusamt").innerHTML);
 
-document.getElementById("convertbutton").onclick= function(){
+document.getElementById("convertbutton").onclick= function() {
+  var ctemp = parseInt(document.getElementById("celsius").value);
   temperature = ctemp * (9/5) + 32;
-  document.getElementById('fahamt').innerHTML= temperature;
-  determineBackground (temperature);
+  document.getElementById('fahrenheit').value= temperature;
+  determineBackground(temperature);
 }
 
-function determinebackground (temperature) {
+function determineBackground(temperature) {
   if (temperature < 40) {
-    document.body.style.background= url ("../images/winter.jpg")
+    document.body.style.backgroundImage= 'url("./images/winter.jpg")';
+    console.log(temperature);
   }
   else if (temperature <70) {
-     document.body.style.background= url ("../images/fall.jpg")
+     document.body.style.backgroundImage='url("./images/fall.jpg")';
   }
   else if (temperature <90) {
-     document.body.style.background= url ("../images/beach.jpg")
+     document.body.style.backgroundImage= 'url("./images/beach.jpg")';
   }
   else if (temperature> 91) {
-     document.body.style.background= url ("../images/desert.jpg")
+     document.body.style.backgroundImage= 'url("./images/desert.jpg")';
   }
 }
